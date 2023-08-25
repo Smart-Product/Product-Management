b488588af49f3e1dd4cf6e18c305b8fa27aae90b
Erick_Alan <73358160+erickalandev@users.noreply.github.com>
2023-08-25 17:19:46 -0300

    Delete back-end/gerenciamento directory

--- back-end/gerenciamento/src/main/java/io/github/smart/product/Repository.java
+++
@@ -1,9 +0,0 @@
-package io.github.smart.product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import io.github.smart.product.domain.entity.params.Estado;
-
-public interface Repository extends JpaRepository<Estado, Integer> {
-    
-}
